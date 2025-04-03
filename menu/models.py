917477137e5aaff32cc18b42622cdc6de00ce210
Bhavya Garg <bebhavyagarg@gmail.com>
2025-04-03 11:11:36 +0530

    Added svgs for menu categories

--- menu/models.py
+++ menu/models.py
@@ -12,7 +12,7 @@ class MenuItem(models.Model):
 class Menu(models.Model):
     category = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.CharField(max_length=40)
 
     def __str__(self):
         return self.name
