d8ed5fdb6b98126e79a392d542e88e4477d10493
Bhavya Garg <bebhavyagarg@gmail.com>
2025-04-09 10:43:54 +0530

    Set up .env

--- kitchenette/settings.py
+++ kitchenette/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^kfw_%slx2+sa_$gdt_xg#9f&vi7fd%rje(g(!wy!6ka7_-c_l'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 # Application definition
 
@@ -67,7 +67,7 @@ AUTHENTICATION_BACKENDS = (
 )
 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kitchenette.onrender.com']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 LOGIN_URL = '/accounts/login/'  # Redirect here if not logged in
 LOGIN_REDIRECT_URL = '/menu/'   # Redirect here after login
