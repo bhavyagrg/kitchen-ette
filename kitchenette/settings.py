4adf14eac5f46a0eab5c89059bafa0e00d8da405
Bhavya Garg <bebhavyagarg@gmail.com>
2025-04-10 13:27:22 +0530

    Postgres

--- kitchenette/settings.py
+++ kitchenette/settings.py
@@ -113,16 +113,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'kitchenette.wsgi.application'
 
+import dj_database_url
+import os
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
